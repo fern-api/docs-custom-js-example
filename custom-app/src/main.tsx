0a8389e440c048edcf1f6a5d8a7ae72a68d0487d
Andrew Jiang <andrew@buildwithfern.com>
2024-05-29 14:23:04 -0400

    update main.tsx

--- custom-app/src/main.tsx
+++ custom-app/src/main.tsx
@@ -1,39 +1,14 @@
 import './main.css'
 
-import ReactDOM from 'react-dom'
 import { NavFooter, NavHeader } from './NavHeader'
-import React from 'react'
+import { onDOMContentMutate, renderInContainer } from './utils'
 
 function render() {
-  ReactDOM.render(
-    React.createElement(NavHeader),
-    document.getElementById('fern-header'),
-  )
-  ReactDOM.render(
-    React.createElement(NavFooter),
-    document.getElementById('fern-footer'),
-  )
+  // render custom header
+  renderInContainer(NavHeader, document.getElementById('fern-header'))
+
+  // render custom footer
+  renderInContainer(NavFooter, document.getElementById('fern-footer'))
 }
 
-let observations = 0
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOMContentLoaded')
-  render()
-  new MutationObserver((e, o) => {
-    render()
-    for (const item of e) {
-      if (item.target instanceof HTMLElement) {
-        const target = item.target
-        if (target.id === 'fern-header' || target.id === 'fern-footer') {
-          if (observations < 3) {
-            // react hydration will trigger a mutation event
-            observations++
-          } else {
-            o.disconnect()
-          }
-          break
-        }
-      }
-    }
-  }).observe(document.body, { childList: true, subtree: true })
-})
+onDOMContentMutate(render)
